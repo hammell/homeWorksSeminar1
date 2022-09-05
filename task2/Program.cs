ce2579666fc497275fcc0edfba22ad9ad092a18d
Alexandr <hammell@ya.ru>
2022-09-05 22:56:09 +0800

    DZ1-3

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 333;
+int b = -13;
+int max= a;
+if (b > max) max = b;
+Console.Write ("max = ");
+Console.WriteLine (max);
+
+// проблема в сравнии отрицательных чисел... решено
---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 3;
+int b = 12;
+int c = -5;
+int max= a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write ("max = ");
+Console.WriteLine (max);
